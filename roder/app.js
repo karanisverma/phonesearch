cb2cff548840cdf83a8f0558d5c8227b9d75b7b6
karan.verma <karan.verma@skoov.com>
2016-06-01 02:41:57 +0530

    added slider in template

--- roder/app.js
+++ roder/app.js
@@ -1,19 +1,20 @@
-(function() {
-var app = angular.module('phoneApp', []);
+(function(){
+var app = angular.module('mobile-app', ['ngMaterial']);
 app.controller('phoneAppController', ['$http', function($http) {
-    var pdata = this; // pdata stands for phone data.
-    var test = "yada-yada-yada-yada-yada";
-    $http.get('http://karanverma.me/js/phonedata.json').success(function(data) {
-        pdata = data;
-          
-        console.log("PHONE DATA =>>>> ", pdata);
+    var phoneApp = this;
+    phoneApp.mobile="ya y iaskh dkjldjove bhiukadsfjk"; // pdata stands for phone data.
+    // var test = "yada-yada-yadayada-yada";
+    $http.get('http://karanverma.me/js/mobiledata.json').success(function(data) {
+        phoneApp.mobile = data;  
+        // console.log("PHONE DATA =>>>> ",phoneApp.mobile);
     });
-	
+    
      //closing success data 
 }]); //closing controller
-
-app.controller('testController',function(){
-    var google = "kumbha karan";
-	var wiki = "bhukkad";
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+  $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+  $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
 });
 })();
