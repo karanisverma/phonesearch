cc1306619693fa98ea52cadf69a18f614b4e6dca
karan.verma <karan.verma@skoov.com>
2016-06-01 12:11:54 +0530

    added ng-button but not working

--- roder/app.js
+++ roder/app.js
@@ -2,8 +2,10 @@
 var app = angular.module('mobile-app', ['ngMaterial']);
 app.controller('phoneAppController', ['$http', function($http) {
     var phoneApp = this;
-    phoneApp.mobile="ya y iaskh dkjldjove bhiukadsfjk"; // pdata stands for phone data.
-    // var test = "yada-yada-yadayada-yada";
+    var search = function(rating2,rating1,rating3,rating4){
+        console.log(rating2+", "+rating1+", "+rating3+", "+rating4);
+    }
+    
     $http.get('http://karanverma.me/js/mobiledata.json').success(function(data) {
         phoneApp.mobile = data;  
         // console.log("PHONE DATA =>>>> ",phoneApp.mobile);
