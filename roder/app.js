0a1c7c7d1146839e0e7cff898011ae6a3c8ec37e
karan.verma <karan.verma@skoov.com>
2016-06-02 23:45:54 +0530

    renaming search=>ranker

--- roder/app.js
+++ roder/app.js
@@ -1,18 +1,28 @@
 (function(){
 var app = angular.module('mobile-app', ['ngMaterial']);
-app.controller('phoneAppController', ['$http', '$log', function($http) {
+app.controller('phoneAppController', ['$http', function($http) {
     var phoneApp = this;
-    this.search = function(rating2,rating1,rating3,rating4){
-        // console.log("worked!!");
-        console.log(rating2+", "+rating1+", "+rating3+", "+rating4);
-    }
-    this.dummy = function(){
-        console.log("dummy is working")
-    }
     $http.get('http://karanverma.me/js/mobiledata.json').success(function(data) {
         phoneApp.mobile = data;  
         // console.log("PHONE DATA =>>>> ",phoneApp.mobile);
     });
+    this.ranker = function(rating1,rating2,rating3,rating4){
+        // console.log("worked!!");
+        // console.log("Printing it from"+phoneApp.mobile);
+        console.log(rating2+", "+rating1+", "+rating3+", "+rating4);
+        for (i = 0; i < phoneApp.mobile.length; i++) { 
+            console.log(phoneApp.mobile[i].name);
+}
+        // TODO :
+        // apply for-loop for each product in data 
+        // and get the ranking out of it 
+        // assign it to some value like mobile.rankers
+        // and show it to the user
+    }
+    this.dummy = function(){
+        console.log("dummy is working");
+    }
+
     
      //closing success data 
 }]); //closing controller
