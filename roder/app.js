68d80796e156618d9f6e012fdab2b344a5be6188
karan.verma <karan.verma@skoov.com>
2016-06-02 22:43:44 +0530

    ng-click search is working now

--- roder/app.js
+++ roder/app.js
@@ -1,11 +1,14 @@
 (function(){
 var app = angular.module('mobile-app', ['ngMaterial']);
-app.controller('phoneAppController', ['$http', function($http) {
+app.controller('phoneAppController', ['$http', '$log', function($http) {
     var phoneApp = this;
-    var search = function(rating2,rating1,rating3,rating4){
+    this.search = function(rating2,rating1,rating3,rating4){
+        // console.log("worked!!");
         console.log(rating2+", "+rating1+", "+rating3+", "+rating4);
     }
-    
+    this.dummy = function(){
+        console.log("dummy is working")
+    }
     $http.get('http://karanverma.me/js/mobiledata.json').success(function(data) {
         phoneApp.mobile = data;  
         // console.log("PHONE DATA =>>>> ",phoneApp.mobile);
